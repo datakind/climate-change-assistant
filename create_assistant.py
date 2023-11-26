d62cb176a8fbdc365d2c334486c16e29315980ee
Matthew Harris <matthew@regolith.org>
2023-11-26 08:14:20 -0600

    Removing requirement for bs4

--- create_assistant.py
+++ create_assistant.py
@@ -13,7 +13,6 @@ assistant_id = os.environ.get("ASSISTANT_ID")
 model = os.environ.get("MODEL")
 client = AsyncOpenAI(api_key=api_key)
 
-
 async def create():
     get_pf_data_schema = {
         "name": "get_pf_data",
@@ -54,12 +53,12 @@ async def create():
         "Hello, Climate Change Assistant. You help people understand how climate change will affect their homes"
         "You will use Probable Futures Data to predict climate change indicators for a location"
         "You will summarize perfectly the returned data"
-        "You will also provide links to resources to help the user prepare for the predicted climate change"
-        "Links for the US should include FEMA resources"
+        "You will also provide links to local resources and websites to help the user prepare for the predicted climate change"
         "If you don't have enough address information, request it"
         "You default to warming scenario of 1.5 if not specified, but ask if the user wants to try others after presenting results"
         "Group results into categories"
         "Always link to the probable futures website for the location using URL and replacing LATITUDE and LONGITUDE with location values: https://probablefutures.org/maps/?selected_map=days_above_32c&map_version=latest&volume=heat&warming_scenario=1.5&map_projection=mercator#9.2/LATITUDE/LONGITUDE"
+        "GENERATE OUTPUT THAT IS CLEAR AND EASY TO UNDERSTAND FOR A NON-TECHNICAL USER"
     """
 
     tools = [
